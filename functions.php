27023eb83fc4193926a917bebe59d6d8eeb7cec3
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-10-22 09:23:36 +1300

    edited function

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
 
-function addCustomThemeStyles(){
+function addCustomThemeFiles_1902(){
 
     wp_enqueue_style('customCSS', get_template_directory_uri() . '/assets/css/style.css', array(), '0.0.1', 'all');
 
+    wp_enqueue_script();
+
 };
 
-add_action('wp_enqueue_scripts', 'addCustomThemeStyles');
+add_action('wp_enqueue_scripts', 'addCustomThemeFiles_1902');
