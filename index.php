9294077d11f2ce99bf83476bb22a5a07e36eb481
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-22 11:47:20 +1300

    laying out images inside a card

--- index.php
+++ index.php
@@ -10,14 +10,44 @@
         <div class="container">
             <?php if( have_posts() ): ?>
                 <?php while( have_posts() ): the_post(); ?>
-                    <div class="card mt-3">
+                    <div class="card mt-3 border">
                         <h5 class="card-header"><?php the_title(); ?></h5>
                           <div class="card-body">
-                              <h5 class="card-title">Special Title Treatment</h5>
-                              <div>
-                                  <?php the_content(); ?>
+
+                                <?php if ( is_home() ): ?>
+
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
+                                        </div>
+                                        <div class="col-8">
+                                            <?php if ( is_home() ):?>
+                                                <?php the_excerpt(); ?>
+                                            <?php else: ?>
+                                                <?php the_content(); ?>
+                                            <?php endif; ?>
+
+                                            <?php if (!is_single() ): ?>
+                                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go Somewhere</a>
+                                            <?php endif; ?>
+                                        </div>
+                                        
+                                <?php else: ?>
+
+                                    <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
+                                    <?php if ( is_home() ):?>
+                                        <?php the_excerpt(); ?>
+                                    <?php else: ?>
+                                        <br><br>
+                                        <?php the_content(); ?>
+                                    <?php endif; ?>
+                                    <?php if (!is_single() ): ?>
+                                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go Somewhere</a>
+                                    <?php endif; ?>
+
+                                <?php endif; ?>
+
                               </div>
-                              <a href="#" class="btn btn-primary">Go Somewhere</a>
                           </div>
                     </div>
                 <?php endwhile; ?>
