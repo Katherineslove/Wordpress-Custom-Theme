e760f3cbf2e95c946e425a04e0dd74f7c85d0a74
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-22 09:37:54 +1300

    added a new script line

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function addCustomThemeFiles_1902(){
 
     wp_enqueue_style('customCSS', get_template_directory_uri() . '/assets/css/style.css', array(), '0.0.1', 'all');
 
+    wp_enqueue_script('jquery');
     wp_enqueue_script('customJS1902', get_template_directory_uri() . '/assets/js/script.js', array(), '0.0.1', true);
 
 };
