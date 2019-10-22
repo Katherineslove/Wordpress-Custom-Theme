9fb5af0b58f327fac158fb7c8f5f721d8f6a2ede
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-23 11:50:03 +1300

    working on adding navs to the site

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
             </form>
         </nav>
 
-        <!-- <div class="row dark">
+        <div class="row dark">
           <div class="col-2">
             <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
               <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
@@ -43,7 +43,7 @@
               <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
             </div>
           </div>
-        </div> -->
+        </div>
     <?php endif; ?>
 
 
