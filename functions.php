188be27f955a04bfafe70ea019f2de7781ff0e67
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-23 11:46:42 +1300

    working on adding navs to the site

--- functions.php
+++ functions.php
@@ -18,8 +18,12 @@ add_image_size("icon", 50, 50, true);
 
 function addCustomMenus_1902(){
     add_theme_support('menus');
-    // register_nav_menu('top_navigation', 'The top navigation is located at the top of each page.');
     register_nav_menu('top_navigation', __('The top navigation is located at the top of each page.', '1902Custom'));
 }
-
 add_action('after_setup_theme', 'addCustomMenus_1902');
+
+
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
--- index.php
+++ index.php
@@ -7,15 +7,45 @@
 </head>
 <body>
 
+
     <?php
-        if (has_nav_menu('top_navigation')) {
-            wp_nav_menu(array(
-                "theme_location" => "top_navigation",
-                "menu_class" => "top_menu_class",
-                "menu_id" => "top_menu_id"
-            ));
-        }
-    ?>
+        if (has_nav_menu('top_navigation')): ?>
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">19WDWU02 WORDPRESS</a>
+            <?php wp_nav_menu( array(
+                'theme_location'    => 'top_navigation',
+                'menu_class'        => 'top_menu_class',
+                'menu_id'           => 'top_menu_id',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker(),
+            ) );
+            ?>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+
+        <!-- <div class="row dark">
+          <div class="col-2">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+              <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+              <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+              <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+              <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+            </div>
+          </div>
+        </div> -->
+    <?php endif; ?>
+
 
     <div class="container">
         <?php if( have_posts() ): ?>
