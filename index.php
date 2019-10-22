fe8d90f845bc0587fb8dbe8200c032c0dc2917b3
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-23 08:48:08 +1300

    made the image layouting work

--- index.php
+++ index.php
@@ -1,59 +1,39 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <?php wp_head(); ?>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php wp_head(); ?>
+</head>
+<body>
 
-        <div class="container">
-            <?php if( have_posts() ): ?>
-                <?php while( have_posts() ): the_post(); ?>
-                    <div class="card mt-3 border">
-                        <h5 class="card-header"><?php the_title(); ?></h5>
-                          <div class="card-body">
-
-                                <?php if ( is_home() ): ?>
-
-                                    <div class="row">
-                                        <div class="col-4">
-                                            <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
-                                        </div>
-                                        <div class="col-8">
-                                            <?php if ( is_home() ):?>
-                                                <?php the_excerpt(); ?>
-                                            <?php else: ?>
-                                                <?php the_content(); ?>
-                                            <?php endif; ?>
-
-                                            <?php if (!is_single() ): ?>
-                                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go Somewhere</a>
-                                            <?php endif; ?>
-                                        </div>
-                                        
-                                <?php else: ?>
-
-                                    <?php the_post_thumbnail('full', ['class' => 'img-fluid']); ?>
-                                    <?php if ( is_home() ):?>
-                                        <?php the_excerpt(); ?>
-                                    <?php else: ?>
-                                        <br><br>
-                                        <?php the_content(); ?>
-                                    <?php endif; ?>
-                                    <?php if (!is_single() ): ?>
-                                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go Somewhere</a>
-                                    <?php endif; ?>
-
-                                <?php endif; ?>
-
-                              </div>
-                          </div>
+    <div class="container">
+        <?php if( have_posts() ): ?>
+            <?php while( have_posts() ): the_post(); ?>
+                <div class="card mt-3 shadow">
+                    <h5 class="card-header"><?php the_title(); ?></h5>
+                        <?php if(is_home()): ?>
+                            <div class="card-body d-flex justify-contwnt-between">
+                        <?php elseif(!is_home()):?>
+                            <div class="card-body">
+                        <?php endif; ?>
+                        <div class="mr-3">
+                            <?php the_post_thumbnail('large', ['class' => 'img-fluid']); ?>
+                        </div>
+                        <div>
+                            <?php if(is_home()): ?>
+                                <?php the_excerpt() ?>
+                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go somewhere</a>
+                            <?php else:?>
+                                <?php the_content(); ?>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
-        </div>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 
-        <?php wp_footer(); ?>
-    </body>
+    <?php wp_footer(); ?>
+</body>
 </html>
