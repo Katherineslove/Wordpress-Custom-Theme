eb82d7fd66b65c2eb76e623c00afa1e31b067e9e
Katherine Love <katherine@Katherines-MacBook-Air.local>
2019-10-29 11:50:52 +1300

    added the vide and gallery in

--- templates/content-video.php
+++ templates/content-video.php
@@ -3,19 +3,21 @@
         $allBlocks = parse_blocks(get_the_content());
         for ($i=0; $i < count($allBlocks); $i++) {
 
-            echo "Block number " . $i . " is a " . $allBlocks[$i]['blockName'];
+            // echo "Block number " . $i . " is a " . $allBlocks[$i]['blockName'];
+            // echo "<br>";
 
-            echo "<br>";
-
-            if ($allBlocks[$i]['blockName'] == 'core-embed/youtube') {
-                // code...
+            if ($allBlocks[$i]['blockName'] === 'core-embed/youtube') {
+                // echo 'this is a video block';
+                $firstVideoBlock = $allBlocks[$i];
+                break;
             }
 
             // echo "<pre>";
             //     print_r($allBlocks[$i]);
             // echo "</pre>";
         }
-        die();
+        // var_dump($firstVideoBlock);
+        // die();
     };
  ?>
 
@@ -23,6 +25,13 @@
 <div class="card h-100 border border-warning">
     <div class="card-body">
         <h5 class="card-title"><?php the_title(); ?></h5>
+
+        <?php if ($firstVideoBlock): ?>
+            <div class="fullVideo">
+                <?php echo apply_filters('the_content', render_block($firstVideoBlock)); ?>
+            </div>
+        <?php endif; ?>
+
         <a href="<?php the_permalink(); ?>" class="btn btn-warning btn-block">Watch Video</a>
     </div>
 </div>
